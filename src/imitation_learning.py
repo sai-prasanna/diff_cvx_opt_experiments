45a51aa6c6ec1ba2ad6eb805c342af4107c34aa5
sai-prasanna <sai.r.prasanna@gmail.com>
2023-01-27 12:52:33 +0100

    Initial commit

---
+++ src/imitation_learning.py
@@ -0,0 +1,118 @@
+import random
+import tqdm
+from continuous_cartpole_env import CartPoleEnv
+import torch
+import cvxpy as cp
+from cvxpylayers.torch import CvxpyLayer
+import warnings
+import torch.nn as nn
+import gymnasium as gym
+import numpy as np
+import time
+
+import control
+
+
+def build_lqr_policy(Q, R, A, B):
+   
+    # observation: state. 4D in this case.
+    # x, v, theta, v_theta = observation
+    # cost function 
+    K, S, E = control.lqr(A,B,Q,R)
+    
+    def policy(x):
+        action = -1*np.dot(K, x)
+        return action[0]
+    return policy
+
+
+def build_mpc_control_policy(nx, nu, T, A, B, Q, R):
+
+    x = cp.Variable((nx, T + 1))
+    u = cp.Variable((nu, T))
+    x_0 = cp.Parameter(nx)
+    cost = 0.0
+    constr = []
+    for t in range(T):
+        cost += cp.quad_form(x[:, t + 1], Q)
+        cost += cp.quad_form(u[:, t], R)
+        constr += [x[:, t + 1] == A @ x[:, t] + B @ u[:, t]]
+        constr += [cp.norm(u[:, t], 'inf') <= 1.0]
+    # print(x0)
+    constr += [x[:, 0] == x_0]
+    prob = cp.Problem(cp.Minimize(cost), constr)
+
+    def policy(x0):
+        x_0.value = x0
+        prob.solve(verbose=False)
+        if prob.status != cp.OPTIMAL:
+            warnings.warn("Solver did not converge!")
+            return 0
+        ou = np.array(u.value[0, :]).flatten()
+        return ou[0]
+    return policy
+
+
+def get_model_matrix(env, delta_t=1.0, add_identity=True):
+    m = float(env.masspole)
+    M = float(env.masscart)
+    l_bar = float(env.length)
+    g = float(env.gravity)
+    # Model Parameter
+    A = np.array([
+        [0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, -m * g / M, 0.0],
+        [0.0, 0.0, 0.0, 1.0],
+        [0.0, 0.0, g * (M + m) / (l_bar * M), 0.0]
+    ])
+    A = delta_t * A
+    if add_identity:
+        A += np.identity(4)
+    B = np.array([
+        [0.0],
+        [1.0 / M],
+        [0.0],
+        [-1.0 / (l_bar * M)]
+    ])
+    B = delta_t * B
+
+    return A, B
+
+
+def main():
+    random.seed(42)
+    env = CartPoleEnv()
+    env = gym.wrappers.TimeLimit(env, max_episode_steps=200)
+
+    
+    Q = np.diag([1.0, 1.0, 1.0, 1.0])
+    R = np.diag([1.0])
+    nx = 4
+    nu = 1
+    T = 10
+    control_method = 'mpc'
+    if control_method == 'lqr':
+        A, B = get_model_matrix(env, add_identity=False)
+        policy = build_lqr_policy(Q, R, A, B)
+    else:
+        A, B = get_model_matrix(env, delta_t=1.0, add_identity=True)
+        policy = build_mpc_control_policy(nx, nu, T, A, B, Q, R)
+
+    episode_rewards = []
+    for i in tqdm.tqdm(range(10)):
+        episode_reward = 0
+        state, _ = env.reset()
+        terminated = False
+        truncated = False
+        while not (terminated or truncated):
+            action = policy(state)
+            action = np.clip(action, -1.0, 1.0)
+            state, reward, terminated, truncated, _ = env.step([action])
+
+            episode_reward += reward
+            #env.render()
+        episode_rewards.append(episode_reward)
+    print(np.mean(episode_rewards))
+
+if __name__ == '__main__':
+    main()
